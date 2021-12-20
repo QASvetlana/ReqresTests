b3bb14d009ac1fd64944b7d36f84e9a7c77909d9
Svetlana <89111378147@yandex.ru>
2021-12-21 01:31:52 +0300

    successful and negative authorization

---
+++ src/test/java/ReqresTests.java
@@ -0,0 +1,37 @@
+import io.restassured.http.ContentType;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.is;
+
+public class ReqresTests {
+    @Test
+    void successfulLogin() {
+
+
+        given()
+                .contentType(ContentType.JSON)
+                .body("{\"email\": \"eve.holt@reqres.in\"," +
+                        "\"password\": \"cityslicka\"}")
+                .when()
+                .post("https://reqres.in/api/login")
+                .then()
+                .statusCode(200)
+                .body("token", is("QpwL5tke4Pnpja7X4"));
+
+
+    }
+
+    @Test
+    void negativeLogin() {
+        given()
+                .contentType(ContentType.JSON)
+                .body("{\"email\": \"eve.holt@reqres.in\"}")
+                .when()
+                .post("https://reqres.in/api/login")
+                .then()
+                .statusCode(400)
+                .body("error", is("Missing password"));
+    }
+
+}
